65d9748fc607c4a0e07289dbb741f844c33b8a7a
artugkar00 <atugcekarakaya@gmail.com>
2018-12-14 16:31:39 +0300

    he

---
+++ 01 hello world/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="tugcar";
+            int age = 20;
+            float weight = 63.7f;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Weight: " + weight);
+
+
+        }
+    }
+}

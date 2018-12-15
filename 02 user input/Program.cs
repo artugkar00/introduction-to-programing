65f1793f7bf3a4b138266e70b89a93e4cd84a0c4
artugkar00 <atugcekarakaya@gmail.com>
2018-12-15 14:00:17 +0300

    helo

---
+++ 02 user input/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _02_user_input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We get user's name
+            string userName;
+            Console.WriteLine("hello buddy :) My name is hüdoş. What is your name? ");
+            userName = Console.ReadLine();
+            Console.WriteLine(" Hello "+ userName + " nice to meet you" );
+
+            // We get user's age                                
+            int userAge;
+            Console.WriteLine("How old are you?");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is "+ userAge);
+            respondToAge(userAge);
+
+            // We get user's weight
+            double userWeight;
+            Console.WriteLine("Please write your weight");
+            userWeight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Your weight is "+ userWeight);
+            respondToWeight(userWeight);
+        }
+        static void respondToAge(int age)
+        {
+            if(age<18) 
+            {
+                Console.WriteLine("You are too young :)");
+            } 
+            else if(age>40)
+            {
+                Console.WriteLine("Oww you are too old :(");
+            } 
+            else
+            {
+                Console.WriteLine("Your age is perfect");
+            }
+        }
+
+        static void respondToWeight(double weight)
+        {
+            if(weight<60)
+            {
+                Console.WriteLine("You are too weak");
+            }
+            else if(weight>90)
+            {
+                Console.WriteLine("You are too fat");
+            }
+            else
+            {
+                Console.WriteLine("Your weight is normal");
+            }
+        }
+
+    }
+}

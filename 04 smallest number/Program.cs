5e3e114b4bcbc1cfe1e19d88d0a271e1b578b318
artugkar00 <atugcekarakaya@gmail.com>
2018-12-21 00:40:05 +0300

    found the smallest number

---
+++ 04 smallest number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _04_smallest_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+    
+         { 
+            int size;
+            Console.WriteLine("Enter the size of array");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+              
+              Console.WriteLine("Enter the array elements");
+              array [i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int number = array [0];
+            for (int i = 0; i < number; i++);
+            
+               if (array[0] < number)
+               number= array[0];
+               Console.WriteLine("Smallest number {0}", number);
+               Console.ReadLine();
+        
+         }  
+    }
+}
